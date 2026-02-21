7a45d5c23f32388a7eca3b015d170554c68b8e3a
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2026-02-21 18:11:26 +0900

    add 더미

---
+++ Lab/Leaf-raking/OceanModel.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <d3d11.h>
+#include <directxmath.h>
+#include <memory>
+
+
+class GridModelBuffer;
+class QuadTree;
+class Frustum;
+class WaterShader;
+
+
+class OceanModel {
+public:
+    OceanModel();
+	OceanModel(const OceanModel& other) = delete;
+    ~OceanModel();
+
+    bool Init(ID3D11Device*, int, int, float);
+    void Shutdown();
+    void Render(Frustum*, ID3D11DeviceContext*, WaterShader*);
+
+    DirectX::XMMATRIX GetWorldMatrix();
+
+private:
+    std::unique_ptr<GridModelBuffer> m_modelBuffer;
+    std::unique_ptr<QuadTree> m_quadTree;
+
+    DirectX::XMMATRIX m_worldMatrix;
+}; // OceanModel
